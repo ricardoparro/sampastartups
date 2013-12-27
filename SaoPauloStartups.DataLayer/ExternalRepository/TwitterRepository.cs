f6f49b51e41360071e954b6082d962f1e134f7d3
ricardoparro <ricardo.parro@gmail.com>
2013-12-27 23:19:23 +0000

    added twitter rep

--- SaoPauloStartups.DataLayer/ExternalRepository/TwitterRepository.cs
+++ SaoPauloStartups.DataLayer/ExternalRepository/TwitterRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SaoPauloStartups.DataLayer.ExternalRepository
 {
-    class TwitterRepository
+    public class TwitterRepository : BaseRepository
     {
     }
 }
